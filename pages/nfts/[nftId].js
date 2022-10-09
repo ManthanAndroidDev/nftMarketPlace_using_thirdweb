976460af68ec6f3ff8517367c4e88453c0f77bc5
Manish Choudhary <manishchaudharyjee@gmail.com>
2022-10-09 12:25:01 +0530

    intergrated react query

--- pages/nfts/[nftId].js
+++ pages/nfts/[nftId].js
@@ -1,87 +1,87 @@
-import Header from "../../components/Header";
-import { useEffect, useMemo, useState } from "react";
+import Header from '../../components/Header';
+import { useEffect, useMemo, useState } from 'react';
 import {
-  useAddress,
-  useMarketplace,
-  useNFTs,
-  useContract,
-} from "@thirdweb-dev/react";
-import { useRouter } from "next/router";
-import NFTImage from "../../components/nft/NFTImage.js";
-import GeneralDetails from "../../components/nft/GeneralDetails";
-import ItemActivity from "../../components/nft/ItemActivity";
-import Purchase from "../../components/nft/Purchase";
+   useAddress,
+   useMarketplace,
+   useNFTs,
+   useContract,
+} from '@thirdweb-dev/react';
+import { useRouter } from 'next/router';
+import NFTImage from '../../components/nft/NFTImage.js';
+import GeneralDetails from '../../components/nft/GeneralDetails';
+import ItemActivity from '../../components/nft/ItemActivity';
+import Purchase from '../../components/nft/Purchase';
 
 const style = {
-  wrapper: `flex flex-col items-center container-lg text-[#e5e8eb]`,
-  container: `container p-6`,
-  topContent: `flex`,
-  nftImgContainer: `flex-1 mr-4`,
-  detailsContainer: `flex-[2] ml-4`,
+   wrapper: `flex flex-col items-center container-lg text-[#e5e8eb]`,
+   container: `container p-6`,
+   topContent: `flex`,
+   nftImgContainer: `flex-1 mr-4`,
+   detailsContainer: `flex-[2] ml-4`,
 };
 
 const Nft = () => {
-  // const { address, connectWallet, provider } = useWeb3();
-  const [selectedNft, setSelectedNft] = useState();
-  const [listings, setListings] = useState([]);
-  const router = useRouter();
+   // const { address, connectWallet, provider } = useWeb3();
+   const [selectedNft, setSelectedNft] = useState();
+   const [listings, setListings] = useState([]);
+   const router = useRouter();
 
-  // const nftModule = useMemo(() => {
-  //   if (!provider) return;
+   // const nftModule = useMemo(() => {
+   //   if (!provider) return;
 
-  //   const sdk = new ThirdwebSDK(provider.getSigner());
-  //   return sdk.getNFTModule("0x63F80dA69eF8608A49D8E4883b4114F28DC5d47E");
-  // }, [provider]);
-  // get all NFTs in the collection
-  const { contract } = useContract(
-    "0x63F80dA69eF8608A49D8E4883b4114F28DC5d47E"
-  );
-  const { data: nfts, isLoading: isReadingNfts } = useNFTs(contract);
+   //   const sdk = new ThirdwebSDK(provider.getSigner());
+   //   return sdk.getNFTModule("0x63F80dA69eF8608A49D8E4883b4114F28DC5d47E");
+   // }, [provider]);
+   // get all NFTs in the collection
+   const { contract } = useContract(
+      '0x63F80dA69eF8608A49D8E4883b4114F28DC5d47E',
+   );
+   const { data: nfts, isLoading: isReadingNfts } = useNFTs(contract);
 
-  useEffect(() => {
-    if (!nfts) return;
-    (async () => {
-      const selectedNftItem = nfts.find(
-        (nft) => Number(nft.metadata.id._hex) === Number(router.query.nftId)
-      );
-      setSelectedNft(selectedNftItem);
-    })();
-  }, [nfts]);
+   useEffect(() => {
+      if (!nfts) return;
+      (async () => {
+         const selectedNftItem = nfts.find(
+            nft => Number(nft.metadata.id._hex) === Number(router.query.nftId),
+         );
+         setSelectedNft(selectedNftItem);
+      })();
+   }, [nfts]);
 
-  const marketplace = useMarketplace(
-    "0xE073aAbD1E166Aa23d9562b9D4aB62b57Da9dE9e"
-  );
+   const marketplace = useMarketplace(
+      '0x606879c4a436594Bf66113993B8B65C19675a0C7',
+   );
 
-  useEffect(() => {
-    if (!marketplace) return;
-    (async () => {
-      setListings(await marketplace.getAllListings());
-    })();
-  }, [marketplace]);
+   useEffect(() => {
+      if (!marketplace) return;
+      (async () => {
+         setListings(await marketplace.getActiveListings());
+      })();
+   }, [marketplace]);
 
-  return (
-    <div>
-      <Header />
-      <div className={style.wrapper}>
-        <div className={style.container}>
-          <div className={style.topContent}>
-            <div className={style.nftImgContainer}>
-              <NFTImage selectedNft={selectedNft} />
+   return (
+      <div>
+         <Header />
+         <div className={style.wrapper}>
+            <div className={style.container}>
+               <div className={style.topContent}>
+                  <div className={style.nftImgContainer}>
+                     <NFTImage selectedNft={selectedNft} />
+                  </div>
+                  <div className={style.detailsContainer}>
+                     <GeneralDetails selectedNft={selectedNft} />
+                     <Purchase
+                        isListed={router.query.isListed}
+                        selectedNft={selectedNft}
+                        listings={listings}
+                     />
+                  </div>
+               </div>
+               <ItemActivity />
             </div>
-            <div className={style.detailsContainer}>
-              <GeneralDetails selectedNft={selectedNft} />
-              <Purchase
-                isListed={router.query.isListed}
-                selectedNft={selectedNft}
-                listings={listings}
-              />
-            </div>
-          </div>
-          <ItemActivity />
-        </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default Nft;
